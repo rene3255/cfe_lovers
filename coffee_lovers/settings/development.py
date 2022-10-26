f69ec08804c2111e7fce5f7756d19bcda87bd54a
Rene Silva <rene.scre@gmail.com>
2022-10-26 09:37:04 -0500

    demo

--- coffee_lovers/settings/development.py
+++ coffee_lovers/settings/development.py
@@ -8,7 +8,7 @@ import cloudinary.api
 from .base import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env()
 environ.Env.read_env()
 
--- coffee_lovers/settings/production.py
+++ coffee_lovers/settings/production.py
@@ -1,5 +1,4 @@
 import os
-import environ
 from pathlib import Path
 import dj_database_url
 import cloudinary
