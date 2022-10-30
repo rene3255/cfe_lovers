d2c147ea2b0d1c0b9b00403be473c955493c4675
Rene Silva <rene.scre@gmail.com>
2022-10-29 23:58:34 -0500

    de

--- coffee_lovers/settings/production.py
+++ coffee_lovers/settings/production.py
@@ -3,6 +3,7 @@ import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
