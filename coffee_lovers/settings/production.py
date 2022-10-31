8da9708f0c8c0a6d80ba169ad83d9908631bc032
Rene Silva <rene.scre@gmail.com>
2022-10-30 19:48:00 -0600

    demo

--- coffee_lovers/settings/production.py
+++ coffee_lovers/settings/production.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = tuple(config('ALLOWED_HOSTSS'),) # tuple(env.list('ALLOWED_PROD_HOST'))
+ALLOWED_HOSTS = list(config('ALLOWED_HOSTSS')) # tuple(env.list('ALLOWED_PROD_HOST'))
 
 INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
