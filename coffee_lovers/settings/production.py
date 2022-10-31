477eb70d27dfe408c0cda80516d62e1eb5e670ca
Rene Silva <rene.scre@gmail.com>
2022-10-30 19:22:43 -0600

    deS

--- coffee_lovers/settings/production.py
+++ coffee_lovers/settings/production.py
@@ -22,7 +22,7 @@ environ.Env.read_env()
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUGG', default=False)
+DEBUG = False
 
 ALLOWED_HOSTS = list(config('ALLOWED_HOSTSS')) # tuple(env.list('ALLOWED_PROD_HOST'))
 
