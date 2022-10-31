b0b8753637b970fec741ce17fa7d22b82f9a0528
Rene Silva <rene.scre@gmail.com>
2022-10-30 19:19:27 -0600

    de

--- coffee_lovers/wsgi.py
+++ coffee_lovers/wsgi.py
@@ -12,6 +12,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_lovers.settings.development')
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
