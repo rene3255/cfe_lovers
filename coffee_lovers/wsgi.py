7354a2182930555b218296349fb61e83c4af5502
Rene Silva <rene.scre@gmail.com>
2022-10-30 20:19:04 -0600

    demos

--- coffee_lovers/wsgi.py
+++ coffee_lovers/wsgi.py
@@ -9,6 +9,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_lovers.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_lovers.settings.production')
 
 application = get_wsgi_application()
